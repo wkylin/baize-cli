2fab5b99d519718cb26df2b5f544019af9d124bc
wkylin.w <wkylin.w@mgail.com>
2022-10-24 11:49:33 +0800

    feat: git branch vite

--- bin/utils/links.js
+++ bin/utils/links.js
@@ -2,6 +2,7 @@ const links = new Map([
   ['React', 'https://github.com/wkylin/pro-react-admin'],
 ]);
 
+// https://github.com/wkylin/pro-react-admin#feature-vite
 // module.exports = links;
 
 export default links
